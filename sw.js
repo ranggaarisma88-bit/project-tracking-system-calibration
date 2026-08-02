9d08cf05774c2d374b4cd1dfcda924c861b2d966
ranggaarisma88-bit <ranggaarisma88@gmail.com>
2026-08-02 22:44:14 +0700

    Tracking Calibration

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'cal-track-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  console.log('[SW] Installing service worker...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[SW] Caching assets...');
+      return cache.addAll(ASSETS).catch(err => {
+        console.error('[SW] Cache error:', err);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[SW] Activating service worker...');
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => {
+              console.log('[SW] Deleting old cache:', key);
+              return caches.delete(key);
+            })
+      );
+    })
+  );
+  self.clients.claim();
+  console.log('[SW] Service worker active!');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(response => {
+        if (response.status === 200) {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        }
+        return response;
+      }).catch(() => {
+        console.log('[SW] Offline - serving cached page');
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
